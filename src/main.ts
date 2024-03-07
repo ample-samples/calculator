9dca6c36592e474ab1e6d9bef79eb24c31f3ef70
Todd <todd.griffin61@outlook.com>
2024-03-07 15:45:28 +0000

    added calculator buttons and basic structure

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+/* 
+ * High-level breakdown
+ * The calculator (at a minimum) will:
+ * - Take input from the user using buttons
+ * - Store the input in state and update it as buttons are pressed
+ * - Parse this state and evaluate the result
+ * - Display it to the user
+ *
+ * It will support (at a minimum):
+ * - add subtract multiply and divide
+ * - Indices
+ * - Clear user input area
+ *
+ * optionally:
+ * - Trig functions
+ * - 
+*/
